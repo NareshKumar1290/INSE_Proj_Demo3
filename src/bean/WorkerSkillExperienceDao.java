eecddc85d367e4609cfc29c951a4e3aec6aefdd5
nareshkumar1290 <naresh.kumar1290@yahoo.com>
2016-03-14 22:49:10 -0400

    Data of Workers Expertise and skills being saved into database. Finally Working.

--- src/bean/WorkerSkillExperienceDao.java
+++ src/bean/WorkerSkillExperienceDao.java
@@ -18,8 +18,6 @@ public class WorkerSkillExperienceDao {
 		return status;
 	}
 	
-	//TODO : Update the table of worker - column=availability to Int or String or Varchar
-	//TODO : Skill Set1 has been done, need to do it for all.
 	public static boolean saveSkillSet1(WorkerSkillExperienceBean bean) throws SQLException{
 		boolean status = false;
 		if(con == null){
@@ -38,10 +36,8 @@ public class WorkerSkillExperienceDao {
 		ps.setString(6, "0.5");
 		ps.setInt(7, bean.getLoginId());
 		
-		
 		int one = ps.executeUpdate();
 		status = one == 1 ? true : false;
-		con.commit();
 		
 		return status;
 	}
@@ -53,17 +49,19 @@ public class WorkerSkillExperienceDao {
 		}
 		
 		PreparedStatement ps=con.prepareStatement("insert into worker"
-				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee) "
-				+ "values (?, ?, ?, ?);");
+				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee, availability, credibility, Login_Details_Id) "
+				+ "values (?, ?, ?, ?, ?, ?, ?);");
 		
 		ps.setString(1, bean.getDomainOfWork2());
 		ps.setString(2, bean.getSpecificTask2());
 		ps.setString(3, bean.getYearsOfExperience2());
 		ps.setString(4, bean.getChargedFee2());
+		ps.setString(5, bean.getAvailable());
+		ps.setString(6, "0.5");
+		ps.setInt(7, bean.getLoginId());
 		
 		int one = ps.executeUpdate();
 		status = one == 1 ? true : false;
-		con.commit();
 		
 		return status;
 	}
@@ -75,17 +73,19 @@ public class WorkerSkillExperienceDao {
 		}
 		
 		PreparedStatement ps=con.prepareStatement("insert into worker"
-				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee) "
-				+ "values (?, ?, ?, ?);");
+				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee, availability, credibility, Login_Details_Id) "
+				+ "values (?, ?, ?, ?, ?, ?, ?);");
 		
 		ps.setString(1, bean.getDomainOfWork3());
 		ps.setString(2, bean.getSpecificTask3());
 		ps.setString(3, bean.getYearsOfExperience3());
 		ps.setString(4, bean.getChargedFee3());
+		ps.setString(5, bean.getAvailable());
+		ps.setString(6, "0.5");
+		ps.setInt(7, bean.getLoginId());
 		
 		int one = ps.executeUpdate();
 		status = one == 1 ? true : false;
-		con.commit();
 		
 		return status;
 	}
@@ -97,17 +97,19 @@ public class WorkerSkillExperienceDao {
 		}
 		
 		PreparedStatement ps=con.prepareStatement("insert into worker"
-				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee) "
-				+ "values (?, ?, ?, ?);");
+				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee, availability, credibility, Login_Details_Id) "
+				+ "values (?, ?, ?, ?, ?, ?, ?);");
 		
 		ps.setString(1, bean.getDomainOfWork4());
 		ps.setString(2, bean.getSpecificTask4());
 		ps.setString(3, bean.getYearsOfExperience4());
 		ps.setString(4, bean.getChargedFee4());
+		ps.setString(5, bean.getAvailable());
+		ps.setString(6, "0.5");
+		ps.setInt(7, bean.getLoginId());
 		
 		int one = ps.executeUpdate();
 		status = one == 1 ? true : false;
-		con.commit();
 		
 		return status;
 	}
@@ -118,17 +120,19 @@ public class WorkerSkillExperienceDao {
 		}
 		
 		PreparedStatement ps=con.prepareStatement("insert into worker"
-				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee) "
-				+ "values (?, ?, ?, ?);");
+				+ "(domainOfWork, specificTask, yearsOfExperience, chargedFee, availability, credibility, Login_Details_Id) "
+				+ "values (?, ?, ?, ?, ?, ?, ?);");
 		
 		ps.setString(1, bean.getDomainOfWork5());
 		ps.setString(2, bean.getSpecificTask5());
 		ps.setString(3, bean.getYearsOfExperience5());
 		ps.setString(4, bean.getChargedFee5());
+		ps.setString(5, bean.getAvailable());
+		ps.setString(6, "0.5");
+		ps.setInt(7, bean.getLoginId());
 		
 		int one = ps.executeUpdate();
 		status = one == 1 ? true : false;
-		con.commit();
 		
 		return status;
 	}
